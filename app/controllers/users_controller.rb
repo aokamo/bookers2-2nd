ed88c1236ac2e83bbaf260287a37d370db4f2339
EC2 Default User <ec2-user@ip-172-31-90-75.ec2.internal>
2021-09-29 11:27:50 +0000

    Update: form&info&users/show.htmle

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,16 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
   end
 
   def show
     @user = User.find(params[:id])
-    
+    @book = Book.new
+
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 end
